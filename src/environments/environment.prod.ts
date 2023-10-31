4776e7899659f2989b7dc58674c39bcee7f89736
Alexei Petru <76633311+alexei-petru@users.noreply.github.com>
2023-10-31 11:33:31 +0100

    fix[plugin]: fix11

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   // Change this to false for a real backend.
   useInMemoryWebApi: true,
   hostUrl: '',
-  apiUrl: 'versomart.netlify.app',
+  apiUrl: '',
   resourceUrlInMemoryDB: '',
 };
