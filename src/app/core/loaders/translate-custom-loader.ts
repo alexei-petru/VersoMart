0b927ee4371ce5be0bada43183c0c1cf198c22ed
Alexei Petru <76633311+alexei-petru@users.noreply.github.com>
2023-11-02 18:54:26 +0100

    fix[heroku]:fix10

--- src/app/core/loaders/translate-custom-loader.ts
+++ src/app/core/loaders/translate-custom-loader.ts
@@ -1,13 +1,13 @@
+import { isPlatformServer } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { TranslationsKeys } from '@app/core/models/types';
 import { TranslateLoader } from '@ngx-translate/core';
 import { all } from 'deepmerge';
 import { Observable, zip } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api.service';
-import { TranslationsKeys } from '@app/core/models/types';
 import { environment } from 'src/environments/environment';
-import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
@@ -19,39 +19,40 @@ export class CustomTranslateLoader implements TranslateLoader {
     @Inject(PLATFORM_ID) private platformId: object,
   ) {}
   getTranslation(lang: string): Observable<TranslationsKeys> {
+    if (isPlatformServer(this.platformId)) {
+      console.log(
+        '\x1b[35m%s\x1b[0m',
+        `translate-custom-loader H17:34 L23: 'loader translation lang'`,
+        lang,
+      );
+      console.log(
+        '\x1b[35m%s\x1b[0m',
+        `translate-custom-loader H18:54 L28: 'http server'`,
+        this.http,
+      );
+      const localTransTemp$ = this.http.get<TranslationsKeys>(
+        `${environment.hostUrl}/assets/i18n/${lang}.json`,
+      );
+      localTransTemp$.subscribe((res) => {
+        console.log(
+          '\x1b[35m%s\x1b[0m',
+          `translate-custom-loader H17:37 L31: 'ssr translation'`,
+          res,
+        );
+      });
+    }
+    const apiTrans$ = this.apiService.getLangTranslations(lang);
     const localTrans$ = this.http.get<TranslationsKeys>(
       `${environment.hostUrl}/assets/i18n/${lang}.json`,
     );
-    return localTrans$;
-    //   if (isPlatformServer(this.platformId)) {
-    //     console.log(
-    //       '\x1b[35m%s\x1b[0m',
-    //       `translate-custom-loader H17:34 L23: 'loader translation lang'`,
-    //       lang,
-    //     );
-    //     const localTransTemp$ = this.http.get<TranslationsKeys>(
-    //       `${environment.hostUrl}/assets/i18n/${lang}.json`,
-    //     );
-    //     localTransTemp$.subscribe((res) => {
-    //       console.log(
-    //         '\x1b[35m%s\x1b[0m',
-    //         `translate-custom-loader H17:37 L31: 'ssr translation'`,
-    //         res,
-    //       );
-    //     });
-    //   }
-    //   const apiTrans$ = this.apiService.getLangTranslations(lang);
-    //   const localTrans$ = this.http.get<TranslationsKeys>(
-    //     `${environment.hostUrl}/assets/i18n/${lang}.json`,
-    //   );
-    //   return zip([apiTrans$, localTrans$]).pipe(
-    //     map(([apiTrans, localTrans]) => {
-    //       const transLocalAndApiMerged = all([localTrans, apiTrans.keys]) as TranslationsKeys;
-    //       return transLocalAndApiMerged as TranslationsKeys;
-    //     }),
-    //     catchError(() => {
-    //       return localTrans$;
-    //     }),
-    //   );
+    return zip([apiTrans$, localTrans$]).pipe(
+      map(([apiTrans, localTrans]) => {
+        const transLocalAndApiMerged = all([localTrans, apiTrans.keys]) as TranslationsKeys;
+        return transLocalAndApiMerged as TranslationsKeys;
+      }),
+      catchError(() => {
+        return localTrans$;
+      }),
+    );
   }
 }
