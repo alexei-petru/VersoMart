b4f7f153708a5a027b7f1a6a527f65a07ecdc538
Alexei Petru <76633311+alexei-petru@users.noreply.github.com>
2023-11-02 19:52:17 +0100

    fix[heroku]:fix12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,12 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppGlobalErrorHandler } from './core/global-error-handler';
 import { SsrCookieCustomService } from './core/libraries/custom-ssr-cookie/ssr-cookie-custom.service';
 import { CustomTranslateLoader } from './core/loaders/translate-custom-loader';
-import { DevModule } from './modules/dev/dev.module';
-import { InMemoryWebApiCustomModule } from './modules/in-memory-web-api-custom/in-memory-custom.module';
 import { ApiService } from './services/api.service';
 import { SharedModule } from './shared/shared.module';
 // eslint-disable-next-line no-restricted-imports
