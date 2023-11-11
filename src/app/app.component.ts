5e9ce719df61552805f43121c1015d7393384136
Alexei Petru <76633311+alexei-petru@users.noreply.github.com>
2023-11-11 17:03:38 +0100

    fix[code-verificationpage]: fix placeholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, Inject, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Renderer2, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { AuthService } from './services/auth.service';
+import { CookieAppService } from './services/cookie-app.service';
 import { LanguageService } from './services/language.service';
 import { SidenavService } from './services/sidenav.service';
 import { ThemeService } from './services/styling/theme.service';
-import { CookieAppService } from './services/cookie-app.service';
-import { AuthService } from './services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -22,8 +21,6 @@ export class AppComponent implements AfterViewInit {
 
   constructor(
     private renderer: Renderer2,
-    @Inject(DOCUMENT) private document: Document,
-    private appTranslate: LanguageService,
     private sidenavService: SidenavService,
     private themeService: ThemeService,
     private language: LanguageService,
@@ -32,18 +29,11 @@ export class AppComponent implements AfterViewInit {
     private router: Router,
   ) {
     this.authService.setActor();
-    this.language.initTranslationLanguage();
-    this.updatePageLang();
+    this.language.initTranslationLanguage(this.renderer);
     this.themeService.setDefaultTheme(this.renderer);
   }
 
   ngAfterViewInit(): void {
     this.sidenavService.setSidenav(this.sidenav);
-  }
-
-  private updatePageLang() {
-    this.appTranslate.languageApp$.subscribe((langObj) => {
-      this.renderer.setAttribute(this.document.documentElement, 'lang', langObj.value);
-    });
   }
 }
